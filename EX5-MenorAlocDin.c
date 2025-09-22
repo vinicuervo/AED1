79c0825cb174b17f792ddef5e639fe0e01b66257
vinicuervo <vinicius.ccuervo@gmail.com>
2025-09-22 09:15:35 -0300

    Add files via upload

---
+++ EX5-MenorAlocDin.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n, v[1000], menor, posicao;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", v + i); 
+        if (i == 0){
+            menor = *(v + i);
+            posicao = i;
+        }
+        else if (*(v + i) < menor){
+            menor = *(v + i);
+            posicao = i;
+        }
+    }
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", posicao);
+    return 0;
+}
